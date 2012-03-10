519b903437b741eddb75028f9f0378cc3f5b7ccd
Brian <bman@bman-VirtualBox.(none)>
2012-03-09 20:34:21 -0500

    modifed added visual to SOLID check

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,35 @@
 
 void something(void);
 
+float coords[2][3] = {{0,0,0},{0,-2,0}};
+
+DtObjectRef obj1,obj2;
+
+void buttonPress(unsigned char key, int x, int y){
+	switch(key){
+		case 'w':
+			coords[0][1]++;
+			break;
+		case 's':
+			coords[0][1]--;
+			break;
+		case 'a':
+			coords[0][0]++;
+			break;
+		case 'd':
+			coords[0][0]--;
+			break;
+		case 'q':
+			coords[0][2]++;
+			break;
+		case 'e':
+			coords[0][2]--;
+			break;
+	
+	}
+	glutPostRedisplay();
+}
+
 void display(void){
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -25,7 +54,40 @@ void display(void){
 	glVertex3f(0.0f, 0.0f, 3.0f);
 	glEnd();
 
-	glRotatef(45, 1.0f,1.0f,0.0f);
+//	glRotatef(45, 1.0f,1.0f,0.0f);
+
+
+	glColor3f(1.0f,0.0f,1.0f);
+	glPushMatrix();
+	glTranslatef(coords[0][0],coords[0][1],coords[0][2]);
+	glRotatef(0,0,1,0);
+	glutWireCube(2.0f);
+	glPopMatrix();
+	
+//	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(coords[1][0],coords[1][1],coords[1][2]);
+	glRotatef(0,0,1,0);
+	glutWireSphere(1.0f,10,10);
+	glPopMatrix();
+
+
+	dtSelectObject(&obj1);
+	dtLoadIdentity();
+	dtTranslate(coords[0][0],coords[0][1],coords[0][2]);
+	dtRotate(0,0,1,0);
+	
+	dtSelectObject(&obj2);
+	dtLoadIdentity();
+	dtTranslate(coords[1][0],coords[1][1], coords[1][2]);
+	dtRotate(0,0,1,0);
+
+
+	if(dtTest()) printf("one {%f, %f, %f}, {%f,%f,%f}\n", coords[0][0],coords[0][1],coords[0][2],
+			coords[1][0], coords[1][1], coords[1][2]);
+	else printf("zero {%f, %f, %f}, {%f,%f,%f}\n", coords[0][0],coords[0][1],coords[0][2],
+			coords[1][0], coords[1][1], coords[1][2]);
+
 	glFlush();
 }
 
@@ -59,29 +121,20 @@ int main (int argc, char ** argv){
 	glutInitWindowSize(500,500);
 	glutInitWindowPosition(100,100);
 	glutCreateWindow("Window title");
+	glutKeyboardFunc(buttonPress);
 
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 
-	DtShapeRef box1 = dtSphere(2.0);
-	DtObjectRef obj1,obj2;
+	DtShapeRef box1 = dtBox(2.0,2.0,2.0);
+	DtShapeRef sphere1 = dtSphere(2.0);
 	dtCreateObject(&obj1, box1);
-	dtCreateObject(&obj2, box1);
-
-
-	dtSelectObject(&obj1);
-	dtLoadIdentity();
-	dtTranslate(0,2,0);
-	dtRotate(0,0,1,0);
+	dtCreateObject(&obj2, sphere1);
 	
-	dtSelectObject(&obj2);
-	dtLoadIdentity();
-	dtTranslate(0,-2,0);
-	dtRotate(0,0,1,0);
+
+
 
  	dtSetDefaultResponse(collide1, DT_SIMPLE_RESPONSE, stdout);
 
-	if(dtTest()) printf("gave a one\n");
-	else printf("gave a zero\n");
 	glutMainLoop();
 }
